b363ecf916f7ba2f411157356cc4d7c91c6105ae
thakurzaildarrana <thakurzaildarrana@gmail.com>
2021-03-14 11:09:02 -0700

    final

--- src/main/java/helix/MatrixHelixMorph.java
+++ src/main/java/helix/MatrixHelixMorph.java
@@ -9,7 +9,74 @@ public class MatrixHelixMorph {
      * @param inMatrix
      * @return a matrix that is morphed into a helix version of inMatrix
      */
-    public static int[][] helix( int[][] inMatrix) {
+    public static int[][] helix( int[][] inMatrix) 
+    {
+    	
+    	
+    	int M =inMatrix.length;
+        int N =inMatrix[0].length;
+        
+        int top = 0, bottom = M - 1;
+        int left = 0, right = N - 1;
+ 
+        int index = 0;
+        
+        
+        int arr[] = new int[M*N];
+        for(int i = 0; i < M; i++) {
+            int[] r = inMatrix[i];
+            for(int j = 0; j < r.length; j++) {
+                int number = inMatrix[i][j];
+                arr[i*r.length+j] = number;
+            }
+        }
+ 
+        while (true)
+        {
+            if (left > right) {
+                break;
+            }
+ 
+            // print top row
+            for (int i = left; i <= right; i++) {
+                inMatrix[top][i] = arr[index++];
+            }
+            top++;
+ 
+            if (top > bottom) {
+                break;
+            }
+ 
+            // print right column
+            for (int i = top; i <= bottom; i++) {
+            	inMatrix[i][right] = arr[index++];
+            }
+            right--;
+ 
+            if (left > right) {
+                break;
+            }
+ 
+            // print bottom row
+            for (int i = right; i >= left; i--) {
+            	inMatrix[bottom][i] = arr[index++];
+            }
+            bottom--;
+ 
+            if (top > bottom) {
+                break;
+            }
+ 
+            // print left column
+            for (int i = bottom; i >= top; i--) {
+            	inMatrix[i][left] = arr[index++];
+            }
+            left++;
+        }
+        
+    	
+    	
+    	
     	  return inMatrix;
 }
     }
--- src/test/java/helix/MatrixHelixMorphTest.java
+++ src/test/java/helix/MatrixHelixMorphTest.java
@@ -42,6 +42,13 @@ public class MatrixHelixMorphTest {
 
         return true;
     }
+    
+    @Test
+    public void test1x1() {
+        int[][] inMatrix  = { {1} };
+        int[][] expOutput = { {1} };
+        assertTrue(testEqual(expOutput, MatrixHelixMorph.helix(inMatrix)));
+    }
 
     @Test
     public void test2x2() {
@@ -50,18 +57,27 @@ public class MatrixHelixMorphTest {
         assertTrue(testEqual(expOutput, MatrixHelixMorph.helix(inMatrix)));
     }
 
-    @Test
-    public void test1x1() {
-        int[][] inMatrix  = { {1} };
-        int[][] expOutput = { {1} };
-        assertTrue(testEqual(expOutput, MatrixHelixMorph.helix(inMatrix)));
-    }
-
+   
     // ADD TESTS TO CHECK PARTITIONS
     @Test
     public void test3x3() {
         int[][] inMatrix  = { {1, 2, 3}, {4,5,6},{7,8,9} };
         int[][] expOutput = { {1, 2, 3}, {8,9,4},{7,6,5} };
         assertTrue(testEqual(expOutput, MatrixHelixMorph.helix(inMatrix)));
+        
+    }
+    @Test
+    public void test4x4() {
+        int[][] inMatrix  = { {1, 2, 3,4}, {5,6,7,8},{9,10,11,12},{13,14,15,16} };
+        int[][] expOutput = { {1, 2, 3,4}, {12,13,14,5},{11,16,15,6},{10,9,8,7} };
+        assertTrue(testEqual(expOutput, MatrixHelixMorph.helix(inMatrix)));
+        
+    }
+    @Test
+    public void test5x5() {
+        int[][] inMatrix  = { {1, 2, 3,4,5}, {6,7,8,9,10},{11,12,13,14,15},{16,17,18,19,20},{21,22,23,24,25} };
+        int[][] expOutput = { {1, 2, 3,4,5}, {16,17,18,19,6},{15,24,25,20,7},{14,23,22,21,8},{13,12,11,10,9} };
+        assertTrue(testEqual(expOutput, MatrixHelixMorph.helix(inMatrix)));
+        
     }
 }
